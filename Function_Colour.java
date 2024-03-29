461b6b2ef3f649b0dd6bb136876fae5ab6ca76d7
Owen Oliveira <119921389+OwenAtConestoga@users.noreply.github.com>
2024-03-29 11:10:36 -0400

    Create Function_Color.java
    Uploaded content of function color file

---
+++ Function_Colour.java
@@ -0,0 +1,47 @@
+package standaard;
+
+
+import java.awt.Color;
+
+public class Function_Color {
+	
+	GUI gui;
+	
+	public Function_Color(GUI gui) {
+		
+		this.gui = gui;
+		
+		
+	}
+	
+	public void changeColor(String color) {
+		
+		switch(color) {
+		case "White": 
+			gui.window.getContentPane().setBackground(Color.white);
+			gui.textArea.setBackground(Color.white);
+			gui.textArea.setForeground(Color.black);
+			break;
+		case "Black":
+			gui.window.getContentPane().setBackground(Color.black);
+			gui.textArea.setBackground(Color.black);
+			gui.textArea.setForeground(Color.white);
+			break;
+		case "Gray":
+			gui.window.getContentPane().setBackground(new Color(32, 32, 32));
+			gui.textArea.setBackground(new Color(32, 32, 32));
+			gui.textArea.setForeground(Color.white);
+			break;
+		case "Terminal":
+			gui.window.getContentPane().setBackground(Color.black);
+			gui.textArea.setBackground(Color.black);
+			gui.textArea.setForeground(Color.green);
+			break;
+		}
+		
+		
+	}
+
+} 
+
+
